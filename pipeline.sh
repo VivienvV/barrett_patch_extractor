38a67846618178fbbcf96285ce85df70ac7b0e45
BPdeRooij <bpderooij@gmail.com>
2022-02-23 01:22:55 +0100

    pipeline amc ready

--- pipeline.sh
+++ pipeline.sh
@@ -1,2 +1,2 @@
-python wsi2biopsy.py --root_dir 'TIFFs' --out_dir 'data/Barrett20x' --magnification 20 --extract_stroma --verbose --save_fig
-python biopsy2patches.py --root_dir 'data/Barrett20x' --out_dir 'data/Barrett20x_patched' --patch_size 224 224 --stride 112 112 --threshold 0.15 --verbose --save_gif
+python wsi2biopsy.py --root_dir '../../../data/archief/AMC-data/Barrett' --out_dir '../../../data/ml/AMC-data/Barrett/Barrett20x' --magnification 20 --extract_stroma --verbose
+python biopsy2patches.py --root_dir '../../../data/ml/AMC-data/Barrett20x' --patch_size 224 224 --stride 112 112 --threshold 0.33 --verbose --dont_save_patches --dataset_probing
